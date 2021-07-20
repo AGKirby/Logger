5d7712f348f6694604288e664e5ed774bb65019b
tobyloki5 <tobyloki5@gmail.com>
2021-07-19 17:58:57 -0700

    updated timestamps

--- logger.js
+++ logger.js
@@ -1,15 +1,15 @@
 exports.info = (msg) => {
-	console.log(`${new Date()} - INFO: ${msg}`);
+	console.log(`${new Date().toISOString()} - INFO: ${msg}`);
 };
 
 exports.warn = (msg) => {
-	console.log(`${new Date()} - WARN: ${msg}`);
+	console.log(`${new Date().toISOString()} - WARN: ${msg}`);
 };
 
 exports.error = (msg) => {
-	console.log(`${new Date()} - ERROR: ${msg}`);
+	console.log(`${new Date().toISOString()} - ERROR: ${msg}`);
 };
 
 exports.debug = (msg) => {
-	console.log(`${new Date()} - DEBUG: ${msg}`);
+	console.log(`${new Date().toISOString()} - DEBUG: ${msg}`);
 };
